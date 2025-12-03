2709d3a644f0f39e52df519b6b8d68e9d1f37924
Samikshya Lamsal <your-email@example.com>
2025-12-03 11:54:17 +0545

    Initial commit: Added Web API and MVC projects

---
+++ FinMate.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinMate.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
